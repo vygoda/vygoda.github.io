faf2de2ae39d2abcf27790f7ea7be8e01cb00042
Eugene Nikulesku <enikulesku@productengine.com>
2015-05-26 10:46:40 +0300

    Adding youtube directive

--- js/directives.js
+++ js/directives.js
@@ -1,3 +1,12 @@
 'use strict';
 
 /* Directives */
+
+phonecatApp.directive('youtube', function() {
+    return {
+        templateUrl: 'partials/youtube-thumbnail.html',
+        scope:{
+            videoId:'@youtube'
+        }
+    };
+});
