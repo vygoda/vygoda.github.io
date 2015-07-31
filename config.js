b147dc84661a0cc4f6a200052188471edc32e179
enikulesku <kc051best@gmail.com>
2015-07-31 14:35:02 +0300

    Adding config

---
+++ config.js
@@ -0,0 +1,16 @@
+"use strict";
+
+angular.module("config", [])
+    .constant("ENV", {
+        "host": "https://api.backendless.com/v1",
+        "application-id": "B216603E-9B55-A43F-FFB5-996A1A0BE900",
+        "secret-key": "7264DFA7-9715-713A-FFF0-DEAEFA16AA00",
+        "youtube-key": "AIzaSyDCznZmYhdRjYBMcmD8c_V6YdTcaot_1p4",
+        "youtube-playlistId": "PLA2CFC4E6D8D01EB4",
+        "flickr-api_key": "a9ed5e5be75dc8d02c768948ab924689",
+        "flickr-user_id": "130413297@N03",
+        "flickr-posters_album_id": "72157651695647255",
+        "googleDrive-folder_id" : "0B-7EtES7S1ZKfmticGh3a3FNSUd3X0NiWktKZUFuRzRGZzQ4bUNlWE8xOWh4VjZUbVh0UDQ",
+        "facebook-app_id" : "773644762748334",
+        "vk-app_id" : "5007540"
+    });
