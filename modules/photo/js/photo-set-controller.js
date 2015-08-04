79c33b82db5096f907d7beffc724afd5f11fc92b
Eugene Nikulesku <enikulesku@productengine.com>
2015-08-04 18:14:16 +0300

    breadcrumbs for photo

--- modules/photo/js/photo-set-controller.js
+++ modules/photo/js/photo-set-controller.js
@@ -3,8 +3,17 @@
 angular.module('vygoda-photo')
 
 .controller('PhotoSetCtrl',
-    function ($scope, PhotoSet, photoSetIds, ENV) {
+    function ($scope, $routeParams, PhotoSet, PhotoCollection, photoSetIds, ENV) {
         $scope.user_id = ENV["flickr-user_id"];
+
+        PhotoCollection.query({}, function(successData) {
+            console.log(successData);
+            successData.collections.collection.forEach(function(item, i, arr) {
+                    if (item.id == $routeParams.collectionId) {
+                        $scope.collectionTitle = item.title;
+                    }
+                });
+        });
 
         $scope.photoSetIds = photoSetIds;
         $scope.photoSets = PhotoSet.query();
