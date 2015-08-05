fb281a2369cb391fed849539a7b5fd132372c006
Eugene Nikulesku <enikulesku@productengine.com>
2015-08-05 13:09:13 +0300

    Adding ability to copy raw html of post

--- js/app.js
+++ js/app.js
@@ -270,4 +270,18 @@ angular.module('vygoda-web', [
 .filter('md', function() {
   var converter = new Showdown.converter();
   return converter.makeHtml;
-});
+})
+
+.directive('selectOnClick', ['$window', function ($window) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            element.on('click', function () {
+                if (!$window.getSelection().toString()) {
+                    // Required for mobile Safari
+                    this.setSelectionRange(0, this.value.length)
+                }
+            });
+        }
+    };
+}]);
