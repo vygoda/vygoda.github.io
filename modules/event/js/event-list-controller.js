7871a827c05727d3edbc9f6c7438a7f9d0524949
Eugene Nikulesku <enikulesku@productengine.com>
2015-08-10 11:15:05 +0300

    Adding ability to pin event

--- modules/event/js/event-list-controller.js
+++ modules/event/js/event-list-controller.js
@@ -6,6 +6,24 @@ angular.module('vygoda-event')
     function ($scope, $routeParams, Event, $location, $timeout, ENV) {
         $scope.pageSize = ENV.pageSize;
 
+        $scope.togglePin = function(event, pinedEvents) {
+            pinedEvents.forEach(function(item, i, arr) {
+                item.pined = false;
+            });
+
+            event.pined = !event.pined;
+
+            Event.update({"eventId": event.eventId}, event);
+
+            Event.query({where: "pined=true"}, function(successData) {
+                successData.data.forEach(function(item, i, arr) {
+                    item.pined = false;
+
+                    Event.update({"eventId": item.eventId}, item);
+                });
+            });
+        };
+
        var calculateOffset = function(page) {
             if (!page) {
                 return 0;
@@ -15,10 +33,12 @@ angular.module('vygoda-event')
         };
 
         var queryItems = function() {
-            $scope.events = Event.query({offset: calculateOffset($routeParams.page)}, function(response) {
+            $scope.events = Event.query({offset: calculateOffset($routeParams.page), where: "pined=false OR pined IS NULL"}, function(response) {
                 $scope.totalCount = response.totalObjects;
                 $scope.page = $routeParams.page;
             });
+
+            $scope.pinedEvents = Event.query({where: "pined=true"});
         };
 
         $scope.pageChanged = function() {
