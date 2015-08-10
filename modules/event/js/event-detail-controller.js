dc79b9245e53e7c0e09a363324ca3c8ede2a000f
Eugene Nikulesku <enikulesku@productengine.com>
2015-08-10 11:46:18 +0300

    Using vk comments by default

--- modules/event/js/event-detail-controller.js
+++ modules/event/js/event-detail-controller.js
@@ -3,9 +3,11 @@
 angular.module('vygoda-event')
 
 .controller('EventDetailCtrl',
-    function ($scope, $routeParams, Event) {
+    function ($scope, $routeParams, $location, Event) {
         $scope.detailed = true;
 
         $scope.event = Event.get({eventId: $routeParams.eventId});
+
+        VK.Widgets.Comments('vk_comments', {}, $location.path().hashCode());
     }
 );
