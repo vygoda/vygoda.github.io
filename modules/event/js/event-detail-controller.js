aa8f298bc78b74769463ee87b4c031b2c396793a
Eugene Nikulesku <nikulesku@ukr.net>
2015-09-02 20:54:44 +0300

    Pretty urls

--- modules/event/js/event-detail-controller.js
+++ modules/event/js/event-detail-controller.js
@@ -6,10 +6,10 @@ angular.module('vygoda-event')
     function ($scope, $routeParams, $location, Event) {
         $scope.detailed = true;
 
-//        Event.get({where: "title LIKE '" + $routeParams.eventTitle.replace(/'/g, "_").replace(/"/g, "_") + "'"}, function(successData) {
-//            $scope.event = successData.data[0];
-//        });
-        $scope.event = Event.get({eventId: $routeParams.eventId});
+        Event.get({where: "(prettyTitle='" + $routeParams.eventId + "')OR(objectId='" + $routeParams.eventId + "')"}, function(successData) {
+            $scope.event = successData.data[0];
+        });
+        //$scope.event = Event.get({eventId: $routeParams.eventId});
 
         VK.Widgets.Comments('vk_comments', {}, $location.path().hashCode());
     }
